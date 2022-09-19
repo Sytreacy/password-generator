f2de3cb7444378c9db0146a1a573e4e75f05017e
Win Maw <sytreacy@gmail.com>
2022-09-19 21:02:21 +0630

    generate main feature and add copy-on-click feature
    - generate 2 random passwords
    - copy on clicking upon the passwords automatically with success message "copied"

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-//generate 2 random passwords when the user clicks the button
-//each pw should be 15 chars long
-
 //stretch goals
 //ability to set password length
-//add "copy on click"
 //toggle "symbols" and "numbers" on/off
+
 const characters = [
   "A",
   "B",
@@ -98,3 +95,51 @@ const characters = [
   "?",
   "/",
 ];
+
+function randomResult() {
+  let randomNo = Math.floor(Math.random() * characters.length);
+  return characters[randomNo];
+}
+
+function generatePwd() {
+  let pwd = "";
+  for (let i = 0; i < 15; i++) {
+    pwd += randomResult();
+  }
+  return pwd;
+}
+
+function generatePwds() {
+  pwd1.textContent = generatePwd();
+  pwd2.textContent = generatePwd();
+  pwd1.addEventListener("click", copyText);
+  pwd2.addEventListener("click", copyText);
+}
+
+let generatePwdBtn = document.getElementById("generate-pwd");
+generatePwdBtn.addEventListener("click", generatePwds);
+
+let pwd1 = document.getElementsByClassName("password-1")[0];
+let pwd2 = document.getElementsByClassName("password-2")[0];
+
+//"copy on click"
+function copyText(event) {
+  const cb = navigator.clipboard; //navigator : navigator object is used for browser detection. The Clipboard object used to access the system clipboard.
+  const textToCopy = event.currentTarget;
+  cb.writeText(textToCopy.textContent).then(() => copySuccess(event)); //.writeText writes the specified text string to the system clipboard.
+}
+
+function copySuccess(event) {
+  let searchMsg = event.target.parentElement.parentElement; //pw-box-container
+
+  // to not get spammed with "copied" text many times
+  if (searchMsg.children.length !== 2) {
+    let textSuccess = document.createElement("p");
+    textSuccess.classList.add("copy-success");
+    event.target.parentElement.parentElement.appendChild(textSuccess);
+    textSuccess.textContent = "copied!";
+    setTimeout(() => {
+      textSuccess.remove(); // remove the success text after 1 second
+    }, 1000);
+  }
+}
